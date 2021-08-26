c8a5cdfe225d01182f017cf0a908fc79b16f1670
Sumesh <sumeshnaskar27@gmail.com>
2021-08-26 23:12:00 +0530

    added feautures to browser container

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link as RouterLink} from 'react-router-dom' 
 
 import { 
@@ -7,11 +7,17 @@ import {
     Logo, 
     ButtonLink,
     Group,
+    Search,
+    SearchIcon,
+    SearchInput,
     Text,
     Link,
     Feature,
     FeatureCallOut,
-    PlayButton
+    PlayButton,
+    Profile,
+    Picture,
+    Dropdown
 } from './styles/header'
 
 export default function Header({ bg=true, children, ...restProps }){
@@ -24,6 +30,22 @@ Header.Frame = function HeaderFrame({  children, ...restProps }){
 
 Header.Group = function HeaderGroup({ children, ...restProps }) {
     return <Group {...restProps}>{children}</Group>
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }){
+    const [searchActive, setSearchActive] = useState(false)
+    return (
+        <Search { ...restProps }>
+            <SearchIcon onClick = { () => setSearchActive(!searchActive) }>
+                <img src="/images/icons/search.png" alt="Search"/>
+            </SearchIcon> 
+            <SearchInput 
+                value = {searchTerm}
+                onChange = { ({ target }) => setSearchTerm(target.value)}
+                placeholder = "Search films and series"
+                active = {searchActive} />
+        </Search>
+    )
 }
 
 Header.Logo = function HeaderLogo({  to, ...restProps }){
@@ -57,4 +79,20 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }
 
 Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
     return <PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+    return (
+        <Profile {...restProps}>
+            {children}
+        </Profile>
+    )
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+}
+
+Header.Dropdown = function HeaderDRopdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
 }
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -35,6 +35,7 @@ export const Link = styled.p`
     &:hover {
         font-weight: bold;
     }
+
     &:last-of-type {
         margin-right: 0;
     }
@@ -119,5 +120,104 @@ export const PlayButton = styled.button`
     &:hover {
         background: #ff1e1e;
         color: white;
+    }
+`
+
+export const Search = styled.div`
+    display: flex;
+    align-items: center;
+
+    @media(max-width: 700px){
+        display: none;
+    }
+`
+export const SearchIcon = styled.button`
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
+
+    img{
+        width: 16px;
+        filter: brightness(0) invert(1);
+    }
+`
+export const SearchInput = styled.input`
+    background-color: #44444459;
+    color: white;
+    border: 1px solid white;
+    transition: width 0.5s;
+    height: 30px;
+    font-size: 14px;
+    margin-left: ${({active}) => active ? '10px' : '0'};
+    padding: ${({ active }) => active ? '0 10px' : '0'};
+    opacity: ${({ active }) => active ? '1' : '0'}; 
+    width: ${({ active }) => active ? '200px' : '0'};
+`
+export const Picture = styled.button`
+    background: url(${({ src }) => src});
+    background-size: contain;
+    border: 0;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+`
+export const Dropdown = styled.div`
+    display: none;
+    position: absolute;
+    background-color: black;
+    padding: 10px;
+    width: 100px;
+    top: 32px;
+    right: 10px;
+
+    ${Group}:first-of-type ${Link}, ${Picture} {
+        max-width: 50%;
+        font-size: 50%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-weight: normal;
+    }
+
+    ${Group}:last-of-type ${Link} {
+        cursor: pointer;
+    }
+
+    ${Group} {
+        margin-bottom: 10px;
+        
+        &:last-of-type {
+            margin-bottom: 0;
+        }
+        
+        ${Link}, ${Picture} {
+            cursor: default;
+        }
+
+    }
+
+    button {
+        margin-right: 10px;
+    }
+
+    p {
+        font-size: 12px;
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+`
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    position: relative;
+
+    button {
+        cursor: pointer;
+    }
+
+    &:hover > ${Dropdown} {
+        display: flex;
+        flex-direction: column;
     }
 `
