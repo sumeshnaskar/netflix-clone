168ecde027e2d506a8d7aab4c769feaf3ab495a5
SUMESH NASKAR <81361688+sumeshnaskar@users.noreply.github.com>
2021-08-29 21:27:07 +0530

    Broken image fixed (#10)
    * feature and header components added
    
    * firebase setup, sign in and sign up page
    
    * firebase auth, new pages and componenet
    
    * added feautures to browser container
    
    * hydrate firestore and display all data
    
    * project completed
    
    * deployment dependencies added
    
    * broken image fixed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -37,7 +37,7 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
     return (
         <Search { ...restProps }>
             <SearchIcon onClick = { () => setSearchActive(!searchActive) }>
-                <img src="/images/icons/search.png" alt="Search"/>
+                <img src={process.env.PUBLIC_URL + "/images/icons/search.png"} alt="Search"/>
             </SearchIcon> 
             <SearchInput 
                 value = {searchTerm}
@@ -90,7 +90,7 @@ Header.Profile = function HeaderProfile({ children, ...restProps }) {
 }
 
 Header.Picture = function HeaderPicture({ src, ...restProps }) {
-    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+    return <Picture {...restProps} src={process.env.PUBLIC_URL + `/images/users/${src}.png`} />;
 }
 
 Header.Dropdown = function HeaderDRopdown({ children, ...restProps }) {
