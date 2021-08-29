75281bd1188ad1492122295aafa24c87f7f59a35
Sumesh <sumeshnaskar27@gmail.com>
2021-08-29 20:38:38 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import * as ROUTES from './constants/routes'
 import { Browse, Home, Signin, Signup } from './pages'
+import { useAuthListener } from './hooks'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+
 // import { seedDatabase } from './seed'
 // import { FirebaseContext } from './context/firebase'
 
 function App() {
   // const { firebase } = useContext(FirebaseContext)
   // seedDatabase(firebase)
+
+  const { user } = useAuthListener()
+  console.log(user)
   return (
     <Router>
       <Switch>
-        <Route path={ROUTES.BROWSE}><Browse /></Route>
-        <Route path={ROUTES.SIGN_UP}><Signup/></Route>
-        <Route path={ROUTES.SIGN_IN}><Signin/></Route>
-        <Route path={ROUTES.HOME}><Home/></Route>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse/>
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <Signup/>
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <Signin/>
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home/>
+        </IsUserRedirect>
       </Switch>
     </Router>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,5 @@ export { default as Feauture } from './feature'
 export { default as Form } from './form'
 export { default as Profiles } from './profiles'
 export { default as Loading } from './loading'
+export { default as Player } from './player'
 
